15ce7683d56189a2336933a412720b9f39cd9978
Chris Cornutt <enygma@phpdeveloper.org>
2018-06-03 08:38:32 -0500

    Adding changes to Auth for RBAC handling

--- src/Auth.php
+++ src/Auth.php
@@ -2,11 +2,15 @@
 
 namespace Psecio\Authorize;
 
+use \Psecio\PropAuth\PolicySet;
+
 class Auth
 {
     public static function build($type, ...$input)
     {
         $type = strtolower($type);
+        $match = Enforcer::MATCH_ALL;
+
         switch($type) {
             case 'acl':
                 $policy = self::buildPolicy('acl', $input[0]);
@@ -14,10 +18,18 @@ class Auth
             case 'permission':
                 $policy = self::buildPolicy('permission', $input[0]);
                 break;
+            case 'rbac':
+                // We need two policies here - one for groups and one for perms
+                $policy = [
+                    self::buildPolicy('rbac/group', $input),
+                    self::buildPolicy('rbac/permission', $input)
+                ];
+                $match = Enforcer::MATCH_ANY;
+                break;
             default:
                 throw new \InvalidArgumentException('Unknown type: '.$type);
         }
-        $enforcer = new \Psecio\Authorize\Enforcer($policy);
+        $enforcer = new Enforcer($policy, $match);
         return $enforcer;
     }
 
@@ -28,9 +40,17 @@ class Auth
             return $input;
         }
 
+        // See if we have a special "path"
+        if (strpos($type, '/') !== false) {
+            $parts = array_map(function($value) {
+                return ucwords(strtolower($value));
+            }, explode('/', $type));
+            $type = implode('\\', $parts);
+        }
+
         // If it's not a policy, build it
         $policyNs = '\\Psecio\\Authorize\Policy\\'.ucwords(strtolower($type));
-        if (!class_Exists($policyNs)) {
+        if (!class_exists($policyNs)) {
             throw new \Exception('Invalid policy type: '.$type);
         }
         $policy = new $policyNs($input);
